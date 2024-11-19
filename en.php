1aef0bfb101161180385b359b0b7109aa16223bd
cyberslut <114898116+whoiscyberslut@users.noreply.github.com>
2024-11-19 14:49:35 +0100

    Create en.php

---
+++ en.php
@@ -0,0 +1,8 @@
+<?php
+// English Content
+$history_title = "Containers";
+$history_paragraph = "The idea of using some type of shipping container was not completely novel. Boxes similar to modern containers had been used for combined rail- and horse-drawn transport in England as early as 1792. The US government used small standard-sized containers during the Second World War, which proved a means of quickly and efficiently unloading and distributing supplies.";
+
+$container_industry_title = "Container Industry";
+$container_industry_paragraph = "The container industry has been a cornerstone of global trade for decades, and its evolution is far from over. With the rise of digital technologies, the next wave of innovation promises to revolutionize logistics and shipping. From autonomous trucks to smart containers, the future of the industry lies in increased efficiency, sustainability, and security. As we continue to embrace technological advancements, the container industry will open new doors for global commerce and pave the way for a more connected world.";
+?>
