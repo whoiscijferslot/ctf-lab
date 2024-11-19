d4cd032ce1305c0dec992aefc9f6efa45868cab2
cyberslut <114898116+whoiscyberslut@users.noreply.github.com>
2024-11-19 14:49:56 +0100

    Create es.php

---
+++ es.php
@@ -0,0 +1,8 @@
+<?php
+// Spanish Content
+$history_title = "Contenedores";
+$history_paragraph = "La idea de usar algún tipo de contenedor de envío no era completamente nueva. Cajas similares a los contenedores modernos ya se habían utilizado para el transporte combinado de trenes y carruajes en Inglaterra desde 1792. El gobierno de los Estados Unidos utilizó contenedores de tamaño estándar pequeños durante la Segunda Guerra Mundial, lo que demostró ser un medio para descargar y distribuir suministros de manera rápida y eficiente.";
+
+$container_industry_title = "Industria del Contenedor";
+$container_industry_paragraph = "La industria de los contenedores ha sido una piedra angular del comercio global durante décadas, y su evolución está lejos de haber terminado. Con el auge de las tecnologías digitales, la próxima ola de innovación promete revolucionar la logística y el transporte. Desde camiones autónomos hasta contenedores inteligentes, el futuro de la industria radica en una mayor eficiencia, sostenibilidad y seguridad. A medida que seguimos adoptando avances tecnológicos, la industria de los contenedores abrirá nuevas puertas al comercio global y allanará el camino hacia un mundo más conectado.";
+?>
